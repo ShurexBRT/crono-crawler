1f53c4534c4849a8eb9d18720762898e18e2647f
Pedja Jovanovic <58230377+ShurexBRT@users.noreply.github.com>
2026-08-11 16:11:32 +0200

    Enable production load optimization

--- src/game/phaser/config.ts
+++ src/game/phaser/config.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import './scenePolish';
 import './productionArt';
+import './productionLoadOptimization';
 import './productionBackdropRefinement';
 import './productionInteractables';
 import './productionFx';
